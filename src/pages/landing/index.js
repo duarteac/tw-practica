4a722c66f182a4e01f214ebb087beb548185f5c1
AndresD <andresd@pop-os.localdomain>
2019-09-23 13:23:03 -0500

    fixes

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 
 const Landing = () =>(
-<h1>holass</h1>
-
+   
+        <Button variant = "contained"> press me </Button>
+   
 )
 export default Landing;
